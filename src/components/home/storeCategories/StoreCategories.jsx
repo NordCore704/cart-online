c6db35447d301eff04b71728a3a3f1bb68e54609
Simon Peter <petersimon0730@gmail.com>
2023-04-17 01:47:34 +0100

    layout fix 2

--- src/components/home/storeCategories/StoreCategories.jsx
+++ src/components/home/storeCategories/StoreCategories.jsx
@@ -61,7 +61,7 @@ const StoreCategories = () => {
   }, []);
   return (
     <section
-      className={`store--categories h-screen p-3 flex items-center justify-center gap-5 lg:gap-10 ${inter.className} overflow-hidden`}
+      className={`store--categories h-screen p-3 flex items-center justify-center gap-5 lg:gap-10 ${inter.className}`}
       ref={bodyRef}
     >
       <div
